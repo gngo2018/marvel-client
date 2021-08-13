675752104482ed511bc1f6bfb819174e3c70943b
George <gngo2018@gmail.com>
2021-08-12 20:36:25 -0400

    Scaffolded out header and body styling

---
+++ components/layout/Header.tsx
@@ -0,0 +1,8 @@
+import headerCss from './header.module.css'
+export default function Header(){
+    return(
+        <>
+            <div className={headerCss.header}>Marvel Hero Tracker!</div>
+        </>
+    );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import utilsCss from '../styles/utils.module.css'
 import type { AppProps } from 'next/app'
+import Header from '../components/layout/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <div className={utilsCss.app_body}>
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
 export default MyApp
